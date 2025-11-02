f6defd1baf36d33a25ac500eef9fb8e963bc19eb
Dev-Fady <fadymobiledev@gmail.com>
2025-11-02 11:33:43 +0200

    add IGenericRepo

---
+++ CleanArchitecture.Domain/Interfaces/IGenericRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface IGenericRepo<TModels> where TModels : class
+    {
+        IQueryable<TModels> GetAll();
+        Task<TModels> GetByIdAsync(int id);
+        Task<bool> AddAsync(TModels entity);
+        Task<bool> UpdateAsync(TModels entity);
+        Task<bool> ArchivedAsync(TModels entity);
+        Task<bool> UnArchivedAsync(TModels entity);
+    }
+}
