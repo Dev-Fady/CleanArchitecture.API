98cfb1be3a0b3fe0c7d73495f8215e41a6203297
Dev-Fady <fadymobiledev@gmail.com>
2025-11-02 11:33:24 +0200

    add models

---
+++ CleanArchitecture.Domain/Models/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public string? CreatedByID { get; set; }
+        public string? CreatedByName { get; set; }
+        public DateTime? CreatedDateTime { get; set; }
+
+        public string? ModifiedByID { get; set; }
+        public string? ModifiedByName { get; set; }
+        public DateTime? ModifiedDateTime { get; set; }
+
+        public string? ArchivedByID { get; set; }
+        public string? ArchivedByName { get; set; }
+        public DateTime? ArchivedDateTime { get; set; }
+
+        public bool IsArchived { get; set; } = false;
+    }
+}
---
+++ CleanArchitecture.Domain/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Models
+{
+    public class Product : NamedEntity
+    {
+        public decimal BuyPrice { get; set; }
+        public decimal SellPrice { get; set; }
+        public double Quantity { get; set; }
+        public int MinQuantity { get; set; }
+
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
