a266b5bbe7697a163f34409e483f793342d3fc00
Dev-Fady <fadymobiledev@gmail.com>
2025-11-02 11:36:15 +0200

    add Auto Mapping

---
+++ CleanArchitecture.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Features.Commands.CategoryCommands;
+using CleanArchitecture.Application.Features.Dtos.Responses.CategoryResponses;
+using CleanArchitecture.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category,CategoryReadResponseDto>().ReverseMap();
+            CreateMap<Category,CategoryAddCommand>().ReverseMap();
+            CreateMap<CategoryUpdateCommand, Category>()
+                .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+
+        }
+    }
+}
