69ec52bef823a8c8baea8ca7434945d106c845ca
Dev-Fady <fadymobiledev@gmail.com>
2025-11-02 11:41:34 +0200

    Add DI

---
+++ CleanArchitecture.Infrastructure/DI/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.DI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection InfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericRepo<>), typeof(GenericRepo<>));
+
+            return services;
+        }
+    }
+}
