e440a7e31d643cd49b6359d21fffab3aef71d121
Dev-Fady <fadymobiledev@gmail.com>
2025-11-02 11:37:41 +0200

    add Category Read Response Dto

---
+++ CleanArchitecture.Application/Features/Dtos/Responses/CategoryResponses/CategoryReadResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.Dtos.Responses.CategoryResponses
+{
+    public class CategoryReadResponseDto
+    {
+        public int Id { get; set; }
+        public bool IsArchived { get; set; }
+        public string NameAr { get; set; }
+        public string NameEn { get; set; }
+        public string DescriptionAr { get; set; }
+        public string DescriptionEn { get; set; }
+    }
+}
