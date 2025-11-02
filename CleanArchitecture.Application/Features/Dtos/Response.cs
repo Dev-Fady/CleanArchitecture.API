21fce73608e15f01b1ec3f6ce0bb0c1f0c7e7fe5
Dev-Fady <fadymobiledev@gmail.com>
2025-11-02 11:38:00 +0200

    Resoinse

---
+++ CleanArchitecture.Application/Features/Dtos/Response.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.Dtos
+{
+    public class Response<T>
+    {
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public bool IsSuccessded { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.UtcNow;
+
+        public Response() { 
+            DateTime = DateTime.UtcNow;
+
+        }
+        public Response(T data) : this() { 
+            Data = data;
+            IsSuccessded = true;
+        }
+        public Response(string message) : this()
+        {
+            Message = message;
+            IsSuccessded = false;
+        }
+        public Response(T data, string message) : this()
+        {
+            Data = data;
+            Message = message;
+            IsSuccessded = true;
+        }
+    }
+}
