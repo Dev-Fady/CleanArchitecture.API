423217dcc075c47ebba8ad99d36aea27a1ada56e
Dev-Fady <fadymobiledev@gmail.com>
2025-11-02 11:38:34 +0200

    add PaginatedList

---
+++ CleanArchitecture.Application/Features/PaginatedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features
+{
+    public class PaginatedList<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int PageNumber { get; set; }
+
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+
+        public PaginatedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            Items = items;
+            TotalCount = count;
+            PageNumber = pageNumber;
+            TotalPages = count == 0 ? 1 : (int)Math.Ceiling(count / (double)pageSize);
+        }
+
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+    }
+}
