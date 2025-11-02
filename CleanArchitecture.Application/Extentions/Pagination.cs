9699a2687dc9ec4dc966cda801b8fccdd618ac86
Dev-Fady <fadymobiledev@gmail.com>
2025-11-02 11:35:55 +0200

    add PaginateBaseParamter , Pagination

---
+++ CleanArchitecture.Application/Extentions/Pagination.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Extentions
+{
+    public static class Pagination
+    {
+        public static List<T> Paginate<T>(this IQueryable<T> query, int pageNumber, int pageSize)
+        {
+           return query.
+                Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize).ToList();
+        }
+    }
+    public class PaginateBaseParamter
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public PaginateBaseParamter()
+        {
+            PageNumber = 1;
+            PageSize = 10;
+        }
+        public PaginateBaseParamter(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+
+    }
+}
